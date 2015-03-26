0a96380e9196de1ebe090c1a645462a6ae561aba
Wayne Chiu <wayne@delectable.com>
2015-03-25 18:25:55 -0700

    copybara

---
+++ copybara.py
@@ -0,0 +1,30 @@
+import shutil
+import os
+
+
+def copyDir(source, target):
+
+    print "Copying directory:"
+    print source
+    print os.listdir(source)
+
+    #must remove directory first if already exists or else it will error our
+    if(os.path.exists(target)):
+        shutil.rmtree(target)
+    shutil.copytree(source, target)
+
+androidhome = os.path.expanduser('~') + "/AndroidStudioProjects"
+#print os.getcwd()
+#print androidhome
+
+modelsSource = androidhome + "/DelectableAndroid/Delectable/app/src/main/java/com/delectable/mobile/api/models"
+modelsTarget = androidhome + "/MeuApp/app/src/main/java/com/delectable/mobile/api/models"
+
+endpointModelsSource = androidhome + "/DelectableAndroid/Delectable/app/src/main/java/com/delectable/mobile/api/endpointmodels"
+endpointModelsTarget = androidhome + "/MeuApp/app/src/main/java/com/delectable/mobile/api/endpointmodels"
+
+
+copyDir(modelsSource, modelsTarget)
+copyDir(endpointModelsSource, endpointModelsTarget)
+
+
